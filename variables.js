20e153deab224642ed8b813a9e33579b1ca89cd7
Antonio Loureiro <antoniodeveloper@3gmail.com.br>
2021-04-30 17:25:30 -0300

    Aula variables concluída

---
+++ variables.js
@@ -0,0 +1,12 @@
+/* VARIÁVEIS */
+
+// var nome = 'Diego Telles'
+// nome = "Telles"
+
+// let nome = "Diego Telles"
+// nome = 'Teles'
+
+const nome = 'Diego Telles'
+nome = 'Telles'
+
+console.log(nome)
